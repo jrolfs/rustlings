dba4e8acb59bc64e8c4d61cd7afad7516a56f58d
Jamie Rolfs <jamie.rolfs@gmail.com>
2022-11-10 00:08:47 -0800

    chore(progress): box + play around with generics + iterators

--- exercises/standard_library_types/box1.rs
+++ exercises/standard_library_types/box1.rs
@@ -16,12 +16,51 @@
 //
 // Execute `rustlings hint box1` for hints :)
 
+use std::rc::Rc;
+
+use List::{Cons, Nil};
+
 // I AM NOT DONE
 
 #[derive(PartialEq, Debug)]
-pub enum List {
-    Cons(i32, List),
+pub enum List<T: Copy> {
+    Cons(T, Rc<List<T>>),
     Nil,
+}
+
+impl<T: Copy> List<T> {
+    fn iter(self) -> ListIterator<T> {
+        ListIterator::new(self)
+    }
+}
+
+pub struct ListIterator<T: Copy> {
+    current: Rc<List<T>>,
+}
+
+impl<T: Copy> ListIterator<T> {
+    fn new(list: List<T>) -> Self {
+        Self {
+            current: Rc::new(list),
+        }
+    }
+}
+
+impl<T: Copy> Iterator for ListIterator<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let current = Rc::clone(&self.current);
+
+        match &*current {
+            List::Cons(value, next) => {
+                self.current = Rc::clone(next);
+
+                return Option::Some(*value);
+            }
+            Nil => Option::None,
+        }
+    }
 }
 
 fn main() {
@@ -32,13 +71,23 @@ fn main() {
     );
 }
 
-pub fn create_empty_list() -> List {
-    unimplemented!()
+pub fn create_empty_list() -> List<i32> {
+    Nil
 }
 
-pub fn create_non_empty_list() -> List {
-    unimplemented!()
+pub fn create_non_empty_list() -> List<i32> {
+    Cons(5, Rc::new(Cons(4, Rc::new(Nil))))
 }
+
+// struct NumberHolder {
+//     number: i32,
+// }
+
+// impl NumberHolder {
+//     fn new(number: i32) -> Self {
+//         Self { number }
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
@@ -51,6 +100,28 @@ mod tests {
 
     #[test]
     fn test_create_non_empty_list() {
+        assert_ne!(List::Nil, create_non_empty_list());
         assert_ne!(create_empty_list(), create_non_empty_list())
     }
+
+    #[test]
+    fn test_list_iterator() {
+        assert_eq!(
+            vec![5, 4],
+            create_non_empty_list().iter().collect::<Vec<i32>>()
+        )
+    }
+
+    // #[test]
+    // fn test_list_iterator_no_copy() {
+    //     assert_eq!(
+    //         vec![NumberHolder::new(5), NumberHolder::new(4)],
+    //         Cons(
+    //             NumberHolder::new(5),
+    //             Rc::new(Cons(NumberHolder::new(4), Rc::new(Nil)))
+    //         )
+    //         .iter()
+    //         .collect::<Vec<NumberHolder>>()
+    //     )
+    // }
 }
